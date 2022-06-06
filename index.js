a0d725f0784e904693c9a746c77f4211241e0e17
ivanvillan <ivanvillan54@gmail.com>
2022-06-06 19:21:13 -0300

    https domain app

--- index.js
+++ index.js
@@ -9,31 +9,20 @@ const app = express();
 const port = config.port;
 // app.use(express.static(process.cwd()+"/dist/"))
 
-// const whitelist = ['http://localhost:4200', 'http://190.228.175.35', 'http://192.168.248.131',
-//                     'appweb.gisi.com.ar', '192.168.248.131', '190.228.175.35'];
-// const options = {
-//   origin: (origin, callback) => {
-//     if (whitelist.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('no permitido'));
-//     }
-//   }
-// }
-app.use(cors({ origin:true, preflightContinue: true }));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization');
-  if (req.method === "OPTIONS") {
-      return res.status(200).end();
-  } else {
-      next();
+const whitelist = ['http://localhost:4200', 'https://appweb.gisi.com.ar'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
   }
-});
+}
+app.use(cors(options));
 
 require('./utils/');
+
 
 app.use(express.json());
 
